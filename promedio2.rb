66d42f9c6bafeff39e9b9e8ac4b680134fdd49cb
Rafael Quinteros <rafael.quinterosv@gmail.com>
2021-08-17 20:07:43 -0400

    Generando cambios para github

--- promedio2.rb
+++ promedio2.rb
@@ -7,6 +7,6 @@ segundos = [6, 7, 8, 9, 10]
 suma1 = primeros.reduce(0) {|sum1, prim| sum1 + prim}
 suma2 = segundos.reduce(0) {|sum2, seg| sum2 + seg}
 
-puts "La suma de los primeros numeros es: #{suma1}"
+# puts "La suma de los primeros numeros es: #{suma1}"
 puts "La suma de los segundos numeros es: #{suma2}"
 puts "El promedio de ambas sumatorias es: #{(suma1 + suma2) / primeros.length}"
